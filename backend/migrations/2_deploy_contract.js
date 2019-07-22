36311f7e5384ada6ffb5b808e614130e202bc151
Donny <dmacphail1@gmail.com>
2019-07-22 17:13:01 -0400

    updated deploy contract migration

--- backend/migrations/2_deploy_contract.js
+++ backend/migrations/2_deploy_contract.js
@@ -3,6 +3,6 @@ var Lot = artifacts.require("./ParkingLot.sol");
 module.exports = function(deployer, network, accounts) {
 	//console.log('ACCOUNTS: ', accounts);
 	const manager = accounts[0];
-	const minC = 10;
-	deployer.deploy(Lot, minC, manager);
+	const numberOfParkingSpots = 10;
+	deployer.deploy(Lot, manager, numberOfParkingSpots);
 }
