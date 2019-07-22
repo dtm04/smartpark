dfd2aabeb1034b89f9a97de070368c26cf710da3
Donny <dmacphail1@gmail.com>
2019-07-22 15:18:09 -0400

    Fixed invalid address error on deploy

--- backend/migrations/2_deploy_contract.js
+++ backend/migrations/2_deploy_contract.js
@@ -3,7 +3,8 @@ var Lot = artifacts.require("./ParkingLot.sol");
 module.exports = function(deployer, network, accounts) {
 	// TODO: code ParkingLot.sol and all necessary functions
 	// TODO: need valid address to migrate, or change ParkingLot constructor
-	const manager = accounts[3];
+	//console.log('ACCOUNTS: ', accounts);
+	const manager = accounts[0];
 	const minC = 10;
-	deployer.deploy(Lot, manager, minC);
+	deployer.deploy(Lot, minC, manager);
 }
